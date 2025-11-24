1936cc4e3a829a71eacf60797ffd0f2e032dcfa3
fistoWeb <fistotechnical@gmail.com>
2025-11-24 20:05:11 +0530

    update

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from "./App";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <BrowserRouter  basename="/TTP_IML_1">
     <App />
   </BrowserRouter>
 );
